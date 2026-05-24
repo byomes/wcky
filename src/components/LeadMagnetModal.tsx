2765da29bf7623937754fcf77c7916c100dd308a
Dr. William C.K. Yomes <bill.yomes@gmail.com>
2026-05-23 22:34:40 -0400

    Fix lead magnet image crop on mobile

--- src/components/LeadMagnetModal.tsx
+++ src/components/LeadMagnetModal.tsx
@@ -31,12 +31,12 @@ export default function LeadMagnetModal({ isOpen, onClose }: Props) {
         </button>
 
         {/* Cover image */}
-        <div className="relative w-full h-48 sm:h-auto sm:w-2/5 shrink-0">
+        <div className="relative w-full aspect-[4/3] sm:aspect-auto sm:h-auto sm:w-2/5 shrink-0">
           <Image
             src="/images/lead-magnet2.png"
             alt="Study the Bible Like a Pastor"
             fill
-            className="object-cover"
+            className="object-cover object-top"
           />
         </div>
 
